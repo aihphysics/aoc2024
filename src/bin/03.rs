0c6521f08c841dae78acd787b5caad000c2c6487
Alina Hagan <alina-hagan@outlook.com>
2025-02-01 19:32:27 +0000

    day 3 pt2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -14,19 +14,19 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let reg = Regex::new(r"(do\(\)|don't\(\)).*mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+    let reg = Regex::new(r"(do()()\(\)|don\'t()()\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\))").unwrap();
+    let mut enabled = true;
     Some( reg.captures_iter(input)
             .map(|c: Captures| c.extract::<3>())
             .map(|(_, vals)| vals)
-            .fold(0u32, |acc, vals| {
-                println!( "{:?}", vals );
-                if vals[0] == "do()" {
+            .fold(0u32, | acc, vals| {
+                if vals[0] == "do()" { enabled = true; acc }
+                else if vals[0] == "don't()" { enabled = false; acc }
+                else if vals[0][0..3].to_owned() == "mul" && enabled {
                     acc + vals[1].parse::<u32>().unwrap() * vals[2].parse::<u32>().unwrap()
-                } else {
-                    acc
                 }
-            }
-            )
+                else { acc }
+            })
         )
 }
 
